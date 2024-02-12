b88e329effc746ddd515a3718b5f1a9b733bd30b
ekoning2 <ekoning2@illinois.edu>
2024-02-12 15:58:59 -0600

    added behaviorspace runs

---
+++ NothingModel/run_headless.sh
@@ -0,0 +1,4 @@
+NetLogo_Console --headless --model NothingModel.nlogo --experiment size1 
+NetLogo_Console --headless --model NothingModel.nlogo --experiment size10 
+NetLogo_Console --headless --model NothingModel.nlogo --experiment size50
+NetLogo_Console --headless --model NothingModel.nlogo --experiment size100
