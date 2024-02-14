8722c13edd280b0af26eb187fd7fed279b8a2d07
ekoning2 <ekoning2@illinois.edu>
2024-02-14 15:52:21 -0600

    Comm models

---
+++ Communication/run_headless.sh
@@ -0,0 +1,6 @@
+NetLogo_Console --headless --model NothingModel.nlogo --experiment comm_all2all --min-pxcor 0 --min-pycor 0 --max-pxcor 9 --max-pycor 9
+NetLogo_Console --headless --model NothingModel.nlogo --experiment comm_neighborhood --min-pxcor 0 --min-pycor 0 --max-pxcor 9 --max-pycor 9 
+NetLogo_Console --headless --model NothingModel.nlogo --experiment comm_pair_fixed --min-pxcor 0 --min-pycor 0 --max-pxcor 9 --max-pycor 9
+NetLogo_Console --headless --model NothingModel.nlogo --experiment comm_pair_rand --min-pxcor 0 --min-pycor 0 --max-pxcor 9 --max-pycor 9
+NetLogo_Console --headless --model NothingModel.nlogo --experiment comm_report --min-pxcor 0 --min-pycor 0 --max-pxcor 9 --max-pycor 9
+NetLogo_Console --headless --model NothingModel.nlogo --experiment comm_all --min-pxcor 0 --min-pycor 0 --max-pxcor 9 --max-pycor 9
