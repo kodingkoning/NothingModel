0672b8670b9e0696efb3049b83706b3daa9f358a
ekoning2 <ekoning2@illinois.edu>
2024-02-14 10:39:23 -0600

    added Memory

---
+++ Memory/run_headless.sh
@@ -0,0 +1,3 @@
+NetLogo_Console --headless --model NothingModel.nlogo --experiment memory1 
+NetLogo_Console --headless --model NothingModel.nlogo --experiment memory10 
+NetLogo_Console --headless --model NothingModel.nlogo --experiment memory100
